1315197fb079ab9dda034687dbc2bd3a2a18f2c1
PAULA152 <atienzopa152@gmail.com>
2022-05-31 16:34:33 -0700

    Added Schema and Seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO
+    employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Mike', 'Chan', 1, 1),
+    ('Ashley', 'Rodriguez', 2, 2),
+    ('Kevin', 'Tupik', 3, 3),
+    ('Kunal', 'Singh', 4, 4),
+    ('Malia', 'Brown', 5, 5),
+    ('Tom', 'Allen', 6, 6),
+INSERT INTO
+    role (title, salary, dept_id)
+VALUES
+    ('Project Manager', 70000, 1),
+    ('Project Supervisor', 86000, 3),
+    ('Front End Programmer', 65000, 2),
+    ('Back End Programmer', 90000, 2),
+    ('Full-Stack Web Designer', 120000, 2),
+    ('Lawyer', 300000, 4),
+INSERT INTO
+    department (dept_name)
+VALUES
+    ('Human Resources'),
+    ('Software'),
+    ('Marketing'),
+    ('Legal'),
